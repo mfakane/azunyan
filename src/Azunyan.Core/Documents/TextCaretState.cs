3e8a2bc6023079ff8e9b29433523966b98d0fe35
mfakane <509998+mfakane@users.noreply.github.com>
2026-09-02 05:47:23 +0900

    Refactor: Update TextCaretState to use DocumentAnchor

--- src/Azunyan.Core/Documents/TextCaretState.cs
+++ src/Azunyan.Core/Documents/TextCaretState.cs
@@ -7,24 +7,63 @@ namespace Azunyan.Core;
 public readonly record struct TextCaretState
 {
     public TextCaretState(TextSelection selection, int preferredDisplayColumn)
+        : this(
+            selection,
+            preferredDisplayColumn,
+            AnchorAffinity.Before,
+            preferredHorizontalOffset: null)
+    {
+    }
+
+    public TextCaretState(
+        TextSelection selection,
+        int preferredDisplayColumn,
+        AnchorAffinity caretAffinity,
+        double? preferredHorizontalOffset)
     {
         ArgumentOutOfRangeException.ThrowIfNegative(preferredDisplayColumn);
+        if (preferredHorizontalOffset is { } horizontalOffset
+            && (!double.IsFinite(horizontalOffset) || horizontalOffset < 0))
+        {
+            throw new ArgumentOutOfRangeException(nameof(preferredHorizontalOffset));
+        }
 
         Selection = selection;
         PreferredDisplayColumn = preferredDisplayColumn;
+        CaretAffinity = caretAffinity;
+        PreferredHorizontalOffset = preferredHorizontalOffset;
     }
 
     public TextSelection Selection { get; }
 
     public int PreferredDisplayColumn { get; }
 
+    public AnchorAffinity CaretAffinity { get; }
+
+    public double? PreferredHorizontalOffset { get; }
+
     public int CaretPosition => Selection.CaretPosition;
+
+    public DocumentAnchor CaretAnchor => new(
+        new DocumentPosition(CaretPosition),
+        CaretAffinity);
 
     public TextCaretState WithSelection(TextSelection selection) =>
         new(selection, PreferredDisplayColumn);
 
     public TextCaretState WithPreferredDisplayColumn(int column) =>
-        new(Selection, column);
+        new(Selection, column, CaretAffinity, PreferredHorizontalOffset);
+
+    public static TextCaretState CreateNavigation(
+        TextSelection selection,
+        int preferredDisplayColumn,
+        DocumentAnchor caretAnchor,
+        double? preferredHorizontalOffset) =>
+        new(
+            selection,
+            preferredDisplayColumn,
+            caretAnchor.Affinity,
+            preferredHorizontalOffset);
 }
 
 /// <summary>
